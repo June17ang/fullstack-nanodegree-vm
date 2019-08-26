41596c6fd2f80a8139b55591ea646ff5e85539d8
Ang Chun Yee <dev.june01@gmail.com>
2019-08-26 21:03:45 +0800

    Add google oauth login

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -9,6 +9,8 @@ from sqlalchemy import create_engine
 from db_setup import Base, User, Item, ItemCategory
 from sqlalchemy.orm import sessionmaker
 from flask import session as login_session
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
 
 app = Flask(__name__)
 
@@ -19,11 +21,218 @@ Base.metadata.bind = engine
 Session = sessionmaker(bind=engine)
 session = Session()
 
+CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
+
+
 # redirect function
-
-
 def redirect_url(default='home'):
     return request.args.get('next') or request.referrer or url_for(default)
+
+
+# ------------------------------ Login ------------------------------------
+@app.route('/login/')
+def login():
+    """Route to the login page and create anti-forgery state token."""
+
+    state = ''.join(
+        random.choice(
+            string.ascii_uppercase +
+            string.digits) for x in range(32))
+    login_session['state'] = state
+    return render_template("login.html", STATE=state, client_id=CLIENT_ID)
+
+# Redirect to login page.
+@app.route('/')
+@app.route('/category/')
+@app.route('/items/')
+def home():
+    """Route to the homepage."""
+
+    categories = session.query(ItemCategory).all()
+    items = session.query(Item).all()
+    return render_template(
+        'index.html', categories=categories, items=items)
+
+
+# Create state token
+@app.route('/login/')
+def login():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in range(32))
+    login_session['state'] = state
+    return render_template("login.html", STATE=state, client_id=CLIENT_ID)
+
+
+# Connect to the Google Sign-in oAuth method.
+@app.route('/google-connect', methods=['POST'])
+def googleConnect():
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Obtain authorization code
+    code = request.data
+
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is used for the intended user.
+    google_id = credentials.id_token['sub']
+    if result['user_id'] != google_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print("Token's client ID does not match app's.")
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    stored_access_token = login_session.get('access_token')
+    stored_google_id = login_session.get('google_id')
+    if stored_access_token is not None and google_id == stored_google_id:
+        response = make_response(
+            json.dumps('Current user is already connected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['google_id'] = google_id
+
+    # Get user info.
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    # Assing Email as name if User does not have Google+
+    if "name" in data:
+        login_session['username'] = data['name']
+    else:
+        name_corp = data['email'][:data['email'].find("@")]
+        login_session['username'] = name_corp
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    # See if the user exists. If it doesn't, make a new one.
+    user_id = getUserId(data["email"])
+    if not user_id:
+        user_id = createNewUser(login_session)
+    login_session['user_id'] = user_id
+
+    # Show a welcome screen upon successful login.
+    output = ''
+    output += '<h2>Welcome, '
+    output += login_session['username']
+    output += '!</h2>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px; '
+    output += 'border-radius: 150px;'
+    output += '-webkit-border-radius: 150px;-moz-border-radius: 150px;">'
+    flash("You are now logged in as %s!" % login_session['username'])
+    print("Done!")
+    return output
+
+
+# Disconnect Google Account.
+def googleDisconnect():
+    """Disconnect the Google account of the current logged-in user."""
+
+    # Only disconnect the connected user.
+    access_token = login_session.get('access_token')
+    if access_token is None:
+        response = make_response(
+            json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+
+    if result['status'] == '200':
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        response = make_response(
+            json.dumps('Failed to revoke token for given user.'), 400)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+
+# Log out the currently connected user.
+@app.route('/logout')
+def logout():
+    """Log out the currently connected user."""
+
+    if 'username' in login_session:
+        googleDisconnect()
+        del login_session['google_id']
+        del login_session['access_token']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        del login_session['user_id']
+        flash("You have been successfully logged out!")
+        return redirect(redirect_url())
+    else:
+        flash("You were not logged in!")
+        return redirect(redirect_url())
+
+
+# Create new user.
+def createNewUser(login_session):
+    new_user = User(
+        name=login_session['username'],
+        email=login_session['email'],
+        profile_image=login_session['picture']
+    )
+    session.add(new_user)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
+
+
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id=user_id).one()
+    return user
+
+
+def getUserId(email):
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
 
 
 # ------------------------------- CRUD Item -------------------------------
